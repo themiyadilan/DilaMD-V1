9ba33c37e0cc16481ff0f16cccf57100a1504830
themiyadilan <116650623+themiyadilan@users.noreply.github.com>
2024-08-12 11:58:12 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -4,14 +4,15 @@ const {sleep} = require('../lib/functions')
 
 cmd({
     pattern: "restart",
-    desc: "restart the bot",
+    desc: "restart the Dila-MD",
     category: "owner",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+    if(!isowner)return
 const {exec} = require("child_process")
-reply("restarting...")
+reply("Dila-Md restarting... 💤")
 await sleep(1500)
 exec("pm2 restart all")
 }catch(e){
