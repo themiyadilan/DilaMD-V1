8d994ee3419be1a019a59f7be4b9be0d9893fd6b
themiyadilan <116650623+themiyadilan@users.noreply.github.com>
2024-08-18 23:36:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~3Q0QCSpD#TUgs-fbxNHChRwLV68M6r19UYMcm008WskCtTC0dEkE",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/b496e3bdf0f9c068aeb3a.jpg",
     ALIVE_MSG: proceas.env.ALIVE_MSG || "*Hello I'm Alive Now ♥✊*",
 };
